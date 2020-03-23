65713fed0384c1c153ff71a575705a1599a36c95
Krista Tingbrand <kristatingbrand@Kristas-MacBook-Pro.local>
2020-03-23 20:20:26 +0100

    Version 1.0 - command-line executable

--- dataexperimentation.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-data_url = "https://mattblackworld.com/api/countries"
-        
-req = requests.get(data_url)
-response = json.loads(req.text)
-
-print(len(response))
